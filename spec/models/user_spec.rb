3fe2e2063b9676408f65ff5efa378e956ff8b6ba
baracuda19 <alxndrgldbrg@gmail.com>
2013-07-05 01:45:58 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,8 @@ before do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -109,5 +111,8 @@ end
     it { should be_invalid }
    end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -22,13 +22,30 @@ describe "User pages" do
   describe "signup" do
 
     before { visit signup_path }
-
+ 
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      before do
+            fill_in "Name",         with: "Exad__le User"
+            fill_in "Email",        with: "usere.xa.mple.com"
+            fill_in "Password",     with: "fo<.obar"
+            fill_in "Confirmation", with: "fooj,bar"
+          end
+
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+		
+
+    describe "after submission" do
+        	before { click_button submit }
+
+        	it { should have_selector('title', text: 'Sign up') }
+        	it { should have_content('error') }
+      	end
+
     end
 
     describe "with valid information" do
@@ -42,7 +59,22 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+        it { should have_link('Sign out') }
+      end
+    
+
+
     end
+
+
   end
 
 end
