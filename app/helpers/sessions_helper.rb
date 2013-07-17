0a362730c131a63cd3b7ba1374c2247923b36fc3
baracuda19 <alxndrgldbrg@gmail.com>
2013-07-17 02:54:35 -0400

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,14 @@ module SessionsHelper
 
 	def current_user?(user)
         user == current_user
-    end
+        end
+	
+	def signed_in_user
+         unless signed_in?
+          store_location
+          redirect_to signin_url, notice: "Please sign in."
+         end
+        end
 
 	def sign_out
 		self.current_user = nil
@@ -28,7 +35,7 @@ module SessionsHelper
 	def redirect_back_or(default)
     	redirect_to(session[:return_to] || default)
     	session.delete(:return_to)
-    end
+        end
 
   	def store_location
     	session[:return_to] = request.url
